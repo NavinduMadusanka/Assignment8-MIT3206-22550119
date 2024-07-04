b844b9915510a20d2e15d81c2d4aa678649bcbfd
NavinduMadusanka <navindumadusankadias@gmail.com>
2024-07-04 22:31:07 +0530

    Initial Commit

--- app/src/main/java/com/assignment8_mit3206_22550119/MainActivity.java
+++ app/src/main/java/com/assignment8_mit3206_22550119/MainActivity.java
@@ -1,67 +1,77 @@
 package com.assignment8_mit3206_22550119;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends Activity  {
-    Button b1;
-    Button btninternal;
-    EditText ed1;
+import androidx.appcompat.app.AppCompatActivity;
 
-    private WebView wv1;
+public class MainActivity extends AppCompatActivity {
+
+    private EditText urlEditText;
+    private Button browseWebButton;
+    private Button browseLocalFilesButton;
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b1=(Button)findViewById(R.id.button);
-        ed1=(EditText)findViewById(R.id.editText);
-        btninternal=(Button)findViewById(R.id.btninternal);
+        urlEditText = findViewById(R.id.url_edit_text);
+        browseWebButton = findViewById(R.id.browse_web_button);
+        browseLocalFilesButton = findViewById(R.id.browse_local_files_button);
+        webView = findViewById(R.id.web_view);
 
-        wv1=(WebView)findViewById(R.id.webView);
-        wv1.setWebViewClient(new MyBrowser());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
 
-        b1.setOnClickListener(new View.OnClickListener() {
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient());
+
+        browseWebButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String url = ed1.getText().toString();
-
-                wv1.getSettings().setLoadsImagesAutomatically(true);
-                wv1.getSettings().setJavaScriptEnabled(true);
-                wv1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-                wv1.loadUrl(url);
+                String url = urlEditText.getText().toString();
+                if (!url.startsWith("http://") &&!url.startsWith("https://")) {
+                    url = "http://" + url;
+                }
+                webView.loadUrl(url);
             }
         });
 
-        btninternal.setOnClickListener(new View.OnClickListener() {
+        browseLocalFilesButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String base = Environment.getExternalStorageDirectory().getAbsolutePath()
-                        .toString();
-                String imagePath = "file:///"+ base + "/logo.png";
-                String html = "<!DOCTYPE html>";
-                html += "<head><title>Loading files from SDCard</title></head>";
-                html += "<body><img src=\""+ imagePath + "\" />/body>";
-                html += "</html>";
-                wv1.loadDataWithBaseURL("", html, "text/html","UTF-8", null);
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");
+                startActivityForResult(intent, 1);
             }
         });
-
     }
 
-    private class MyBrowser extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+            webView.loadUrl(uri.toString());
         }
-
     }
-
 }
+
+
